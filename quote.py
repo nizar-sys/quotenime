b67a83f04544b6cffdc9682e35d90c6aacb35b68
nizar-sys <raspbery132@gmail.com>
2021-03-08 15:43:09 +0700

    mikasaaaa

---
+++ quote.py
@@ -0,0 +1,19 @@
+
+import requests
+import time
+url = "https://animechan.vercel.app/api/random"
+
+print("\nWellcome to anime quote generator")
+time.sleep(0.3)
+print("Type 'quit' or 'exit' to exit")
+while (True):
+    generate = input("\nPress [Enter] to generate quote ")
+    if generate == 'quit' or generate == 'exit':
+        print("Thanks for use this tool :)")
+        time.sleep(0.5)
+        exit()
+    else:
+        quote = requests.get(url).json()['quote']
+        chara = requests.get(url).json()['character']
+        print("\n")
+        print(quote + "\n" + "- " + chara)
